3dd6f844c7c7ffe4c85e3ce83bafb3795bb38691
Hugo Benitez <hugo.boca16@gmail.com>
2025-04-03 03:59:55 -0300

    cambio de imagen

--- porfolio.js
+++ porfolio.js
@@ -7,3 +7,17 @@ function traducir() {
         elemento.setAttribute("data-en", textoActual); // cambio idioma a ingles
     });
 }
+
+const img = document.querySelector(".hero-img-desarrollador");
+
+img.addEventListener("mouseover", () => {
+    img.style.opacity = "0"; // Se desvanece
+    setTimeout(() => img.src = "imagenes/HugoGhibli.png", 450); // Cambia de imagen
+    setTimeout(() => img.style.opacity = "1", 500); // Se vuelve a mostrar
+});
+
+img.addEventListener("mouseout", () => {
+    img.style.opacity = "0"; // Se desvanece
+    setTimeout(() => img.src = "imagenes/foto1.png", 450); // Vuelve a la imagen original
+    setTimeout(() => img.style.opacity = "1", 500); // Se muestra suavemente
+});
